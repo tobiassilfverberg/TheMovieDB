d6b1ae93938ce8695010e26c77ead0c5ae79bc38
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-18 16:20:11 +0200

    Added get-request to get one movie with id.

--- src/services/MovieAPI.js
+++ src/services/MovieAPI.js
@@ -3,13 +3,19 @@ import axios from 'axios'
 const API_KEY = import.meta.env.VITE_API_KEY
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
 
-// Get whatever is requested
+// Get currently trending movies
 const getCurrentMovies = async () => {
 	return await axios.get(`movie/now_playing?api_key=${API_KEY}`)
 }
 
+// Get one movie 
+const getMovie = async (id) => {
+	return await axios.get(`/movie/${id}?api_key=${API_KEY}`)
+}
+
 const exports = {
-	getCurrentMovies
+	getCurrentMovies,
+	getMovie,
 }
 
 export default exports
