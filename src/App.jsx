b4084df093aaee444c9214f806292146f0f9dfa3
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-18 12:26:18 +0200

    Added Routes to my first three pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import HomePage from './pages/HomePage'
+import CurrentMovies from './pages/CurrentMovies'
+import PopularMovies from './pages/PopularMovies'
+import Toplist from './pages/Toplist'
 import NotFound from './pages/NotFound'
 import './assets/scss/App.scss'
 
@@ -11,6 +14,9 @@ function App() {
 
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/current" element={<CurrentMovies />} />
+				<Route path="/popular" element={<PopularMovies />} />
+				<Route path="/toplist" element={<Toplist />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
