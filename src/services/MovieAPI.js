4ccb89a798f7dd98d641c1c6e01d6f6e771af65e
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-18 13:44:28 +0200

    Added get request for currently trending movies.

--- src/services/MovieAPI.js
+++ src/services/MovieAPI.js
@@ -1,4 +1,15 @@
 import axios from 'axios'
 
-const API_KEY = process.env.API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
+
+// Get whatever is requested
+const getCurrentMovies = async () => {
+	return await axios.get(`movie/now_playing?api_key=${API_KEY}`)
+}
+
+const exports = {
+	getCurrentMovies
+}
+
+export default exports
