60e29319333fe0be6fe413cddcbb1c5be50608da
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-22 10:29:20 +0200

    Pages to get popularMovies and send to 'MovieInfoCard'-component.

--- src/pages/PopularMovies.jsx
+++ src/pages/PopularMovies.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container'
+import usePopularMovies from "../hooks/usePopularMovies"
+import MovieInfoCard from '../components/MovieInfoCard'
 
 const PopularMovies = () => {
-  return (
-    <div>PopularMovies</div>
-  )
+	const { data, isError, error, isLoading } = usePopularMovies()
+
+	return (
+		<Container className="py-3">
+			
+			{isError && error.message}
+
+			{isLoading && (
+				<h2 className="text-center">Loading...</h2>
+			)}
+
+			{data && (
+				<MovieInfoCard movies={data} /> 
+			)}
+
+		</Container>
+	)
 }
 
 export default PopularMovies
