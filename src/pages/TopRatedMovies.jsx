0c49727bceb707f2645d84cce10c763eaa40b619
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-08-22 10:39:58 +0200

    Page to get popular movies and send along to component displaying them.

--- src/pages/TopRatedMovies.jsx
+++ src/pages/TopRatedMovies.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container'
+import useTopRatedMovies from "../hooks/useTopRatedMovies"
+import MovieInfoCard from '../components/MovieInfoCard'
 
-const TopRatedMovies = () => {
-  return (
-    <div>TopRatedMovies</div>
-  )
+const PopularMovies = () => {
+	const { data, isError, error, isLoading } = useTopRatedMovies()
+
+	return (
+		<Container className="py-3 text-center">
+			
+			{isError && error.message}
+
+			{isLoading && (
+				<h2 className="text-center">Loading...</h2>
+			)}
+
+			{data && (
+				<>
+					<h1>Top rated movies</h1>
+					<MovieInfoCard movies={data} /> 
+				</>
+			)}
+
+		</Container>
+	)
 }
 
-export default TopRatedMovies
+export default PopularMovies
